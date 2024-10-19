5465d69da14f990195eb0247755cf79c30fd32b6
Tomas Pereira <tomaspereira2k@gmail.com>
2024-10-19 15:56:08 -0300

    corregido error en api key

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    replace({ preventAssignment: true, values: { 'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY), }, }),replace({ preventAssignment: true, values: { 'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY), }, })
-  ],
+  plugins: [react()]
 })
