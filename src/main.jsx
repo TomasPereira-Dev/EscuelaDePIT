c9286f308572d40df3b3f1e34dcfc5b89b2604e8
Tomas Pereira <tomaspereira2k@gmail.com>
2024-10-26 19:56:18 -0300

    cambio de orden de carpetas para separar server de client

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import Router from './router/Router.jsx';
-import './index.css';
-
-createRoot(document.getElementById('root')).render(
-      <RouterProvider router={Router}/> 
-)
--- src/router/Router.jsx
+++
@@ -1,8 +0,0 @@
-import {  createBrowserRouter } from "react-router-dom";
-import Root from "../Root.jsx";
-
-const router = createBrowserRouter([
-    {path: "*", Component: Root}
-]);
-
-export default router;
